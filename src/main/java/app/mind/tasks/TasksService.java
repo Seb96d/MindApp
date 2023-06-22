397fa9c2b6c824137c5fbdc35dccea2371fa463f
seb96d@gmail.com <seb96d@gmail.com>
2023-06-22 17:30:53 +0200

    implemented REST methods: get post delete(id)
    TODO
    put

--- src/main/java/app/mind/tasks/TasksService.java
+++ src/main/java/app/mind/tasks/TasksService.java
@@ -3,10 +3,8 @@ package app.mind.tasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TasksService {
@@ -27,14 +25,14 @@ public class TasksService {
         return tasksRepository.save(tasks);
     }
 
-    public Tasks saveDetails(Tasks tasks) {
-        return tasksRepository.save(tasks);
+    public void saveDetails(Tasks tasks) {
+        tasksRepository.save(tasks);
     }
 
     public void deleteTask(Long id) {
         if(!tasksRepository.existsById(id)) {
             throw new IllegalStateException(
-                    "task" + id + "does not exist"
+                    "task " + id + "does not exist"
             );
         }
         tasksRepository.deleteById(id);
